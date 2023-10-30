8b8bc6123cc83b66c9d4f87f8031582e37f1a57a
DzXX <janna.lysenko@mail.ru>
2023-10-30 20:52:47 +0300

    the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b ** 2) - (4 * a * c);
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a))
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)); 
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 12) / 100;
+  let payments = amount - contribution;
+  let monthlyPayment = payments * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** countMonths) - 1)));
+  let fullPayment = monthlyPayment * countMonths;
+  return +fullPayment.toFixed(2);
 }
