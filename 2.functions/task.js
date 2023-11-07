e04673ce73d07740cc4254077233b7ccbfbb604a
DzXX <janna.lysenko@mail.ru>
2023-11-08 00:18:31 +0300

    second homework assignment

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,94 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  let avg = sum / arr.length;
+  avg = +((avg).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length < 1) {
+    return 0;
+  }
+  let summElementsWorker = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    summElementsWorker += arr[i];
+  }
+  return summElementsWorker;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length < 1) {
+    return 0
+  }
+  let min = Infinity;
+  let max = -Infinity;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length < 1) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i]
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length < 1) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const max = func(...arrOfArr[i]);
+    if (max > maxWorkResult) {
+      maxWorkResult =max;
+    }
+  }
+  return maxWorkResult;
 }
