23b587e8afc082739d4971326e52a44c5dee1540
DzXX <janna.lysenko@mail.ru>
2023-11-24 19:04:17 +0300

    the fourth task2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,8 +1,8 @@
 function Student(name, gender, age) {
-	this.name = name;
-	this.gender = gender;
-	this.age = age;
-	this.marks = marks;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 let student1 = new Student('Vika', 'female', 18);
@@ -10,30 +10,29 @@ let student2 = new Student('Dima', 'male', 20);
 let student3 = new Student('Liza', 'female', 19);
 let student4 = new Student('Den', 'male', 22);
 
-Student.prototype.setSubject = function(subjectName) {
-	this.subject = subjectName;
+Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function(...marks) {
-	if (this.hasOwnProperty('marks')) {
-		this.marks.push(...marks);
-	}
+    if(this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
+    }
 }
 
-Student.prototype.getAverage = function() {
-	if (this.hasOwnProperty('marks') && this.marks.length !== 0) {
-		let sum = 0;
-		for (let i = 0; i < this.marks.length; i += 1) {
-			sum += this.marks[i];
-		}
-		return sum / this.marks.length;
-	} else {
-		return 0;
-	}
+Student.prototype.getAverage = function () {
+    if(this.hasOwnProperty('marks') && this.marks.length !== 0) {
+        let sum = 0; 
+        for (let i = 0; i < this.marks.length; i += 1) {
+            sum += this.marks[i];}
+            return sum / this.marks.length; 
+    } else {
+        return 0;
+    }
 }
 
-Student.prototype.exclude = function(reason) {
-	delete this.subject;
-	delete this.marks;
-	this.excluded = reason;
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
