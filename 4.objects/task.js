a82754ebbf4d77497d6265ae4938711267919aa5
DzXX <janna.lysenko@mail.ru>
2023-11-24 18:46:33 +0300

    the fourth task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = marks;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student('Vika', 'female', 18);
+let student2 = new Student('Dima', 'male', 20);
+let student3 = new Student('Liza', 'female', 19);
+let student4 = new Student('Den', 'male', 22);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty('marks')) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.hasOwnProperty('marks') && this.marks.length !== 0) {
+		let sum = 0;
+		for (let i = 0; i < this.marks.length; i += 1) {
+			sum += this.marks[i];
+		}
+		return sum / this.marks.length;
+	} else {
+		return 0;
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
